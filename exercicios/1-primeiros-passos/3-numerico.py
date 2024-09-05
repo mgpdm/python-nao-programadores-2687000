7c4021aad40fa71d097dfa43cbe57932b8c303c7
mgpdm <167898942+mgpdm@users.noreply.github.com>
2024-09-05 19:04:47 +0000

    -

--- exercicios/1-primeiros-passos/3-numerico.py
+++ exercicios/1-primeiros-passos/3-numerico.py
@@ -3,9 +3,13 @@ idade_numerica = 46
 altura = 1.74
 
 # Descubra o tipo de dado de cada variável acima
-
-
+print(type(data_nascimento))
+print(type(idade_numerica))
+print(type(altura))
 # Realize uma operação entre dados do tipo string e inteiro
-
+nome ='Matheus'
+print(nome*3)
 
 # Realize uma operação entre dados do tipo inteiro e float
+multiplicacao = idade_numerica * altura
+print(multiplicacao)
