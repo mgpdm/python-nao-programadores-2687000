61211e3297e301fbb0ef12c2101d738552bc003a
mgpdm <167898942+mgpdm@users.noreply.github.com>
2024-09-05 18:25:00 +0000

    -

--- exercicios/2-estruturas-de-dados/1-lista.py
+++ exercicios/2-estruturas-de-dados/1-lista.py
@@ -1,13 +1,20 @@
 # Crie uma lista apenas com elementos numéricos
-
-# Crie uma lista contendo todos os tipos e estrutura de dados que você aprendeu até agora
-
+lista = [1,2,3,78,92]
+print(lista)
+# Crie uma lista contendo todos os tipos e estrutura de dados que você aprendeu 
+# até agora
+lista1 = ['Matheus',4.4, 5,['Citi',3,'Londres','Roma'],0==1,34,5.987]
+print(lista1)
 # Imprima na tela apenas os 5 primeiros elementos da lista
-
+print(lista1[0:5])
 # Crie um slice na lista para que imprima na tela os elementos de índice par
-
+lista2 = [3,'python','yoga',3,9,4,3,'alexa']
+lista3 = lista2[0::2]
+print(lista3)
 # Remova da lista o último item
-
+lista2.pop(-1)
+print(lista2)
 # Insira na lista um novo item
-
+lista4 = lista2.append('mouse')
+print(lista4)
 # Remova da lista um item específico
--- exercicios/2-estruturas-de-dados/projeto-tempo-para-estudos.py
+++ exercicios/2-estruturas-de-dados/projeto-tempo-para-estudos.py
@@ -1,6 +1,27 @@
-# Criaremos um script que imprimirá na tela o total de horas que uma pessoa estudou durante um determinado período:
-# 1. Crie uma variável chamada "nome" e, usando o método input(), atribua a ela um nome;
-# 2. Crie uma variável chamada "total_dias" e, usando o método input(), solicite o total de dias dedicados ao estudo por semana;
-# 3. Crie uma variável chamada "total_horas" e, usanod o método input(), solicite a média de horas estudada por dia;
-# 4. Crie uma variável chamada "curso" e, usando o método input(), solicite o título do curso desejado;
-# 5. Imprima na tela uma frase informando o nome da estudante, o total_dias dedicados aos estudos, o total horas semanais e o curso.
+# Criaremos um script que imprimirá na tela o total de 
+# horas que uma pessoa # estudou durante um determinado 
+# período:
+
+# 1. Crie uma variável chamada "nome" e, usando o método 
+# input(), atribua a ela 
+# um nome;
+nome = input('Digite o seu nome: ')
+
+# 2. Crie uma variável chamada "total_dias" e, usando o 
+# método input(), solicite o total de dias dedicados ao 
+# estudo por semana;
+total_dias = input('Insira o total de dias de estudo: ')
+
+# 3. Crie uma variável chamada "total_horas" e, usando o 
+# método input(), solicite a média de horas estudada por 
+# dia;
+total_horas = input('Insira a média de horas de estudo diária: ')
+
+# 4. Crie uma variável chamada "curso" e, usando o método 
+# input(), solicite o título do curso desejado;
+curso = input('Insira o curso desejado: ')
+
+# 5. Imprima na tela uma frase informando o nome da 
+# estudante, o total_dias dedicados aos estudos, o total 
+# horas semanais e o curso.
+print('O aluno ',nome, '','estudou um total de', total_dias,'dias','e por',total_horas,'horas ', 'no curso de ',curso)
