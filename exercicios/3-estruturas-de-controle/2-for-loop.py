cd017a40c81e1d7b6bb86cd6ed64137f7f7291fd
mgpdm <167898942+mgpdm@users.noreply.github.com>
2024-09-06 02:00:56 +0000

    -

--- exercicios/3-estruturas-de-controle/2-for-loop.py
+++ exercicios/3-estruturas-de-controle/2-for-loop.py
@@ -1,9 +1,16 @@
 # Crie uma lista
-
+carros = ['BMW Z4','Mercedes-Benz','Honda Civic','Ford Ka','Land Rover']
 # Crie um for loop para imprimir cada elemento dessa lista
-
+for carro in carros:
+  print(carro)
 
 # Crie um objeto iterável utilizando a função range()
 
+for numero in range(1,13,3):
+  print(numero)
 
-# Use esse objeto iterável para criar um for loop que imprima na tela a frase "Estou aprendendo uma linguagem de programação."
+# Use esse objeto iterável para criar um for loop que imprima na tela a frase 
+# "Estou aprendendo uma linguagem de programação."
+
+for numero in range(0,13,1):
+  print(numero,'estou aprendendo Python')
