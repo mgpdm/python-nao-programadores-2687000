78f67b58bcf42f1103e077a9dbf37d8d89a8e3e4
mgpdm <167898942+mgpdm@users.noreply.github.com>
2024-09-06 00:14:14 +0000

    -

--- exercicios/3-estruturas-de-controle/1-estrutura-condicional.py
+++ exercicios/3-estruturas-de-controle/1-estrutura-condicional.py
@@ -1,19 +1,29 @@
 # Declare 4 variáveis do tipo numérica
-
+a = float(input('Insira o primeiro número '))
+b = float(input('Insira o segundo número '))
+c = float(input('Insira o terceiro número '))
+d = float(input('Insira o quarto número '))
 
 # Crie uma estrutura condicional para comparar dois números
+if a>b:
+  print('O número', a, 'é maior do que',b)
+else:
+  print('O número',a, 'é menor do que',b)
+
+# Se a condição for verdadeira, imprima na tela uma mensagem informando que a 
+# condição foi cumprida e informando o número de maior valor
 
 
-# Se a condição for verdadeira, imprima na tela uma mensagem informando que a condição foi cumprida e informando o número de maior valor
-
-
-# Se a condição não for cumprida, imprima na tela uma mensagem informando que a condição é negativa e informe o número de maior valor
+# Se a condição não for cumprida, imprima na tela uma mensagem informando que a 
+# condição é negativa e informe o número de maior valor
 
 
 # Insira outras condições na estrutura condicional usando o elif
 
 
-# Incremente a estrutura condicional já existente com expressões lógicas utilizando "and" ou "or"
+# Incremente a estrutura condicional já existente com expressões lógicas utilizando 
+# "and" ou "or"
 
 
-# Crie uma estrutura condicional onde mais de uma condição seja verdadeira, e use apenas a palavra reservada "if"
+# Crie uma estrutura condicional onde mais de uma condição seja verdadeira, e use 
+# apenas a palavra reservada "if"
